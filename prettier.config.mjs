ab82288b71ba36759c20fceb5aa65db4a5546bef
EnricoVacondio <enrico.vacondio@novalab.io>
2026-01-29 16:54:25 +0100

    chore: repository setup

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+export default {
+  endOfLine: "lf",
+  semi: false,
+  printWidth: 130,
+  singleQuote: false,
+  useTabs: false,
+  arrowParens: "always",
+  bracketSameLine: false,
+  trailingComma: "es5",
+}
